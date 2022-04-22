7c97943d8c8b64a50637c7d437258c11810fd74b
Ryan Irelan <ryan@mijingo.com>
2022-04-21 22:05:54 -0500

    deploy script and added rollup config for build

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+const { resolve } = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/house-quest-templates/'
+  base: '/house-quest-templates/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        news: resolve(__dirname, 'news-entry.html')
+      }
+    }
+  }
 })
